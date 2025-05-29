f68f19f77ecdd43562685836439e26a224c31fc2
SJ <ligerssj@naver.com>
2025-05-29 12:00:20 +0900

    b2798 루프 탈출 추가

--- src/b2/b2231/Main.java
+++ src/b2/b2231/Main.java
@@ -9,32 +9,45 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String buff = br.readLine();
 
-        // int n = Integer.parseInt(buff);
-        // int len = buff.length();
+        int n = Integer.parseInt(buff);
+        int len = buff.length();
+        int figure = 1;
         // 자릿수, /= 연산에 쓰려면 문자열 길이보다 하나 작게
-        // int figure = 1;
-        // for(int i = 0; i < len - 1; i++){
-        //     figure *= 10;
+        for (int i = 0; i < len - 1; i++) {
+            figure *= 10;
+        }
+
+        // 각 자리당 가능한 수 2^n개
+        int[] arr = new int[len];
+        int num = n;
+
+
+
+        // for(int i = 0; i < len; i++){
+        // int tmp = num / figure;
+        // while((num - tmp) / figure == tmp) {
+        // if((num - tmp) / figure == tmp) {
+        // num -= tmp;
+        // arr[i] = tmp;
+        // num %= figure;
+        // figure /= 10;
+        // } else {
+        // tmp--;
         // }
-        // // 각 자리당 가능한 값 x - (2^n - 1)
-        // int[] arr = new int[len];
-        // int num = n;
-        // for(int i = 0; num > 0; i++){
-        //     arr[i] = num / figure;
-        //     num %= figure;
-        //     figure /= 10;
         // }
-        // for(int i : arr){
         // }
+        // for(int i : arr)
+        // System.out.print(i);
 
 
         // 문자열로
-        String[] strArr = buff.split("");
+        // String[] strArr = buff.split("");
+        // int num = Integer.parseInt(buff);
 
-        int[] numArr = new int[strArr.length];
-        for(int i = 0; i < strArr.length; i++){
-            numArr[i] = Integer.parseInt(strArr[i]);
-        }
+        // int[] numArr = new int[strArr.length];
+        // for(int i = 0; i < strArr.length; i++){
+        // numArr[i] = Integer.parseInt(strArr[i]);
+        // }
 
 
 
--- src/b2/b2798/Main.java
+++ src/b2/b2798/Main.java
@@ -12,20 +12,28 @@ public class Main {
         int n = Integer.parseInt(buff[0]);
         int m = Integer.parseInt(buff[1]);
 
+        // 값을 배열에 저장하고 정렬
         buff = br.readLine().split(" ");
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(buff[i]);
         Arrays.sort(arr);
 
+        // 가장 큰 값(i)부터 하나씩 올리면서 내부에서 전체적으로 반복
         int max = 0;
-        for(int i = 2; i < n; i++){
-            for(int j = 1; j < i; j++){
+        iLoop: for(int i = 2; i < n; i++){
+            jLoop: for(int j = 1; j < i; j++){
                 for(int k = 0; k < j; k++){
                     int curr = arr[i] + arr[j] + arr[k];
-                    if(!(curr > m) && curr > max){
-                        max = curr;
-                    }
+                    // 목표 값을 넘어버리면 가장 안쪽 루프 스킵
+                    if(curr > m) continue jLoop;
+                    else {
+                        if(curr > max) {
+                            max = curr;
+                            // 목표 값에 도달하면 루프 종료
+                            if(max == m) break iLoop;
+                        }
+                    } 
                 }
             }
         }
