b33f5ed4cb7eccff983cade504f8d466e5446d81
SJ <ligerssj@naver.com>
2025-06-20 17:52:11 +0900

    250620

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int idx = 1;
-        int answer = 0;
-        if (n == 1) {
-            System.out.println(1);
-        } else {
-            for (int i = 1; i < n; i++) {
-                answer += 2;
-                if (answer == Math.pow(2, idx) && i < n - 1) {
-                    answer = 0;
-                    idx++;
-                }
-            }
-            System.out.println(answer);
-        }
+        String buff = br.readLine();
 
+        System.out.println(buff);
     }
 }
--- src/b1/b9506/Main.java
+++ src/b1/b9506/Main.java
@@ -7,14 +7,36 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<List<Integer>> list = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new LinkedHashMap<>();
         int n;
         while ((n = Integer.parseInt(br.readLine())) != -1) {
             List<Integer> tmp = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) tmp.add(i);
+            }
+
+            int sum = 0;
+            for (int num : tmp) {
+                sum += num;
+                if (sum > n) break;
+            }
+            if (sum == n) map.put(n, tmp);
+            else map.put(n, null);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int key : map.keySet()) {
+            if (map.get(key) == null) sb.append(key).append(" is NOT perfect.");
+            else{
+                sb.append(key).append(" = ");
+                List<Integer> list = map.get(key);
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == 0) sb.append(list.get(i));
+                    else sb.append(" + ").append(list.get(i));
+                }
             }
         }
         
+        System.out.println(sb);
     }
 }
