d09a9aef46bbe1ae84c3e1d5120ffe48bb307dbe
SJ <ligerssj@naver.com>
2025-08-11 17:50:48 +0900

    구간 합 구하기 5

--- src/s3/b11659/Main.java
+++ src/s3/b11659/Main.java
@@ -22,36 +22,36 @@ public class Main {
 		int preStart = 0, preEnd = 0;
 		for (int i = 0; i < m; i++) {
 			buff = br.readLine().split(" ");
-			int start = Integer.parseInt(buff[0]);
+			int start = Integer.parseInt(buff[0]) - 1;
 			int end = Integer.parseInt(buff[1]);
 
 			if (i == 0) {
 				for (int j = start; j < end; j++) {
 					sum += arr[j];
 				}
-			}
-
-			if (preStart < start) {
-				for (int j = preStart; j < start; j++) {
-					sum -= arr[j];
+			} else {
+				if (preStart < start) {
+					for (int j = preStart; j < start; j++) {
+						sum -= arr[j];
+					}
 				}
-			}
-
-			if (preEnd > end) {
-				for (int j = end; j < preEnd; j++) {
-					sum -= arr[j];
+	
+				if (preEnd > end) {
+					for (int j = end; j < preEnd; j++) {
+						sum -= arr[j];
+					}
 				}
-			}
-
-			if (start < preStart) {
-				for (int j = start; j < preStart; j++) {
-					sum += arr[j];
+	
+				if (start < preStart) {
+					for (int j = start; j < preStart; j++) {
+						sum += arr[j];
+					}
 				}
-			}
-
-			if (preEnd < end) {
-				for (int j = preEnd; j < end; j++) {
-					sum += arr[j];
+	
+				if (preEnd < end) {
+					for (int j = preEnd; j < end; j++) {
+						sum += arr[j];
+					}
 				}
 			}
 
