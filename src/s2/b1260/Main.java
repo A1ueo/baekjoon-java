9e0d121f9386e9271aaf975ceffce593fd4f497d
SJ <ligerssj@naver.com>
2025-08-29 17:04:16 +0900

    250829

--- src/s2/b1260/Main.java
+++ src/s2/b1260/Main.java
@@ -1,56 +1,71 @@
 package s2.b1260;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 
 public class Main {
 	
-	static Map<String, Set<String>> map;
-	static Set<String> set;
-	static StringBuilder sb;
+	static HashMap<String, Queue<Integer>> map;
+	static StringBuilder sbDfs;
+	static StringBuilder sbBfs;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String[] buff = br.readLine().split(" ");
 		int n = Integer.parseInt(buff[0]);
 		int m = Integer.parseInt(buff[1]);
-		int v = Integer.parseInt(buff[2]);
+		String v = buff[2];
 
-		map = new HashMap<String, Set<String>>(n);
+		map = new HashMap<String, Queue<Integer>>(n);
 		for (int i = 1; i <= n; i++)
-			map.put(String.valueOf(i), new TreeSet<>());
+			map.put(String.valueOf(i), new PriorityQueue<>(Comparator.naturalOrder()));
 
 		for (int i = 0; i < m; i++) {
 			buff = br.readLine().split(" ");
 
-			map.get(buff[0]).add(buff[1]);
-			map.get(buff[1]).add(buff[0]);
+			map.get(buff[0]).add(Integer.valueOf(buff[1]));
+			map.get(buff[1]).add(Integer.valueOf(buff[0]));
 		}
 
-		set = new HashSet<String>();
-		set.add(String.valueOf(v));
-		sb = new StringBuilder();
-		sb.append(v + "\n");
-		method(String.valueOf(v));
+		sbDfs = new StringBuilder();
+		sbDfs.append(v);
+		dfs(v);
+		System.out.println(sbDfs);
 
-		System.out.println(sb);
+		// sbBfs = new StringBuilder();
+		// sbBfs.append(v);
+		// map.get(v).add(-2);
+		// bfs(v);
+		// System.out.println(sbBfs);
 	}
 
-	static void method(String curr) {
+	static void dfs(String curr) {
+		map.get(curr).add("-1");
 		for (String s : map.get(curr)) {
-			sb.append(s + "\n");
-			set.add(s);
-			for (String c : set) {
-				map.get(s).remove(c);
-			}
-			if (!set.contains(s)) {
-				method(s);
+			if (!s.equals("-1") && !map.get(s).contains("-1")) {
+
+				sbDfs.append(" " + s);
+				dfs(s);
 			}
 		}
 	}
+
+	// static void bfs(String curr) {
+	// 	// map.get(curr).add("-2");
+	// 	Queue<String> que = new ArrayDeque<>();
+	// 	for (String s : map.get(curr)) {
+	// 		if (!s.equals("-1") &&
+	// 				!s.equals("-2") && !map.get(s).contains("-2")) {
+	// 			sbBfs.append(" " + s);
+	// 			map.get(s).add("-2");
+	// 			que.add(s);
+	// 		}
+	// 	}
+		
+	// 	for (String s : que) {
+	// 		bfs(s);
+	// 	}
+	// }
 }
